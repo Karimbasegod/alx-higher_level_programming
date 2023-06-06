43490a92c38ede6728787298bec634ec85c95acb
karimbasegod <karimyac2@gmail.com>
2023-06-06 10:20:33 -0700

    commit

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+
+str = "Python is an interpreted, interactive, object-oriented programming
+language that combines remarkable power with very clear syntax"
+start_index = str.find("object-oriented programming")
+end_index = start_index + len("object-oriented programming")
+print(str[start_index:end_index])
